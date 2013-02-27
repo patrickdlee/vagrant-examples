41422dc561547da53e381f34dd9dfaf688475db0
Patrick Lee <alimadzi@gmail.com>
2013-02-27 00:40:24 -0700

    added stylesheets to dynamic-site in examples 5 and 6

--- example5/sites/dynamic-site/application/views/header.php
+++ example5/sites/dynamic-site/application/views/header.php
@@ -1,6 +1,10 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>Vagrant Wish List</title>
+    <link rel="stylesheet" href="/assets/reset.css"/>
+    <link rel="stylesheet" href="/assets/dynamic.css"/>
 </head>
 <body>
+<div id="container">
     <h1>Vagrant Wish List</h1>
